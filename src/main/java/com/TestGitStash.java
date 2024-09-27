b00f117626b034092c3495e39d69ba26a2c49ed9
wangshiwei <a18817792625@163.com>
2024-09-27 10:39:38 +0800

    测试代码

--- src/main/java/com/TestGitStash.java
+++ src/main/java/com/TestGitStash.java
@@ -4,9 +4,9 @@ public class TestGitStash {
 
     public static void main(String[] args) {
         System.out.println("hello world, git 11111111111");
-
-        System.out.println("hello world, git Stashertasp       ");
-
+        System.out.println("hello world, git Stashertasp   123    ");
+        System.out.println("hello world, git Stashertasp  123     ");
+        System.out.println("hello world, git Stashertasp   123    ");
     }
 
 }
